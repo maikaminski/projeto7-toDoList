59327243902f342dce06d1c7cd8e386f0e4f6ef2
LSG-gurgel <loloscheppa@gmail.com>
2025-08-21 21:17:54 -0300

    Function e console.log

--- Projeto7-DEVersidade/src/components/TaskList/Tasklist.tsx
+++ Projeto7-DEVersidade/src/components/TaskList/Tasklist.tsx
@@ -1,33 +1,12 @@
-import React from "react";
-//import TaskItem from "../TaskItem/TaskItem";
-//import styles from './Tasklist.module.css';
-
-// Tipo da tarefa
-type Task = {
+// TaskList.tsx - PARTE 1: Definir a interface das props
+interface Task {
   id: number;
   title: string;
   completed: boolean;
-};
-
-// Tipo das props que o TaskList recebe
-interface TaskListProps {
-  tasks: Task[];
 }
 
-const TaskList: React.FC<TaskListProps> = ({ tasks }) => {
-  return (
-    <div className={styles.taskList}>
-      {tasks.length === 0 ? (
-        <p className={styles.emptyMessage}>
-          Nenhuma tarefa ainda. Adicione uma para começar! ✨
-        </p>
-      ) : (
-        tasks.map((task) => (
-          <TaskItem key={task.id} task={task} />
-        ))
-      )}
-    </div>
-  );
-};
-
-export default TaskList;
+interface TaskListProps {
+  tasks: Task[];         
+  onToggleTask: (id: number) => void;
+  onDeleteTask: (id: number) => void;  
+}
