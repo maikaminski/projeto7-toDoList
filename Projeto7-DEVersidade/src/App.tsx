9d24ac8165e9f28f2d62d77b7de88bbdf58454d6
Rui Gomes <ruisoft01@gmail.com>
2025-08-22 09:31:21 +0100

    Feature/subindo função de remover com dados ficticios

--- Projeto7-DEVersidade/src/App.tsx
+++ Projeto7-DEVersidade/src/App.tsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import TodoList from "./components/TodoList";
+
+type Todo = {
+  id: number;
+  text: string;
+  completed: boolean;
+};
 
 function App() {
+  // alguns todos fictícios
+  const [todos, setTodos] = useState<Todo[]>([
+    { id: 1, text: "Ir ao supermercado", completed: false },
+    { id: 2, text: "Estudar React", completed: false },
+    { id: 3, text: "Treinar no ginásio", completed: true },
+  ]);
+
+  // Função para remover pelo id
+  const removeTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <>
-      
+      <h1>TodoList (SPA)</h1>
+      {/* componente para remover tarefa*/}
+      <TodoList todos={todos} onRemove={removeTodo} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
